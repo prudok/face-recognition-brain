7fbdaf3d5f252bcc1bf8875f1e43e643a365ed05
rel1nce <101862863+rel1nce@users.noreply.github.com>
2022-08-31 12:57:03 +0300

    Updates

---
+++ precache-manifest.0387f06b6e68912765b130df3a2c2cd5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcf95de127859f3cb2b1",
+    "url": "/faceRecognitionBrain/static/css/main.639305e1.chunk.css"
+  },
+  {
+    "revision": "fcf95de127859f3cb2b1",
+    "url": "/faceRecognitionBrain/static/js/main.fcf95de1.chunk.js"
+  },
+  {
+    "revision": "4567e6b4a7bd4ab0b748",
+    "url": "/faceRecognitionBrain/static/js/1.4567e6b4.chunk.js"
+  },
+  {
+    "revision": "32347fb98859e55b6a36",
+    "url": "/faceRecognitionBrain/static/css/2.c8e63d41.chunk.css"
+  },
+  {
+    "revision": "32347fb98859e55b6a36",
+    "url": "/faceRecognitionBrain/static/js/2.32347fb9.chunk.js"
+  },
+  {
+    "revision": "a76d809541e664b56a16",
+    "url": "/faceRecognitionBrain/static/js/runtime~main.a76d8095.js"
+  },
+  {
+    "revision": "41f3e56ba250c3e17bb04896d9fd43a8",
+    "url": "/faceRecognitionBrain/static/media/brain.41f3e56b.png"
+  },
+  {
+    "revision": "2b358518cbc586d1df45df41e748b498",
+    "url": "/faceRecognitionBrain/index.html"
+  }
+];
